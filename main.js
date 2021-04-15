096f44f25726207f85ea340e4f93c2c74e339cdf
Charlesiubao <kristoffer.charles.reyes@gmail.com>
2021-04-15 14:18:45 -0700

    signUp and logIn done

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const url = 'http://localhost:3000/'
 
 //bttn
-const loginBttn = document.querySelector('.loginbutton')
+const loginBttn = document.querySelector('.loginButton')
 const signupBttn = document.querySelector('.signupButton')
 
 
 //forms
-const singupForm = document.querySelector('.signupForm')
+const signupForm = document.querySelector('.signupForm')
 const loginForm = document.querySelector('.loginForm')
 const searchBarForm = document.querySelector('.searchBarForm')
 
 const signupScreen = document.querySelector('.signupScreen')
-const loginScreen = document.querySelector('.lognScreen')
+const loginScreen = document.querySelector('.loginScreen')
 
 
